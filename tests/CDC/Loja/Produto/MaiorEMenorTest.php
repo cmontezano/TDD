558fe91c4da37e8e8b073a4cbdee225244f21135
Carlos Montezano <cmontezanol@gmail.com>
2015-03-17 15:52:44 -0300

    tests passando

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -4,37 +4,31 @@ namespace CDC\Loja\Produto;
 
 require "./vendor/autoload.php";
 
-use CDC\Loja\Carrinho\CarrinhoDeCompras,
-	CDC\Loja\Produto\Produto,
-	CDC\Loja\Produto\MaiorEMenor;
+use CDC\Loja\Carrinho\CarrinhoDeCompras;
+use CDC\Loja\Produto\Produto;
+use CDC\Loja\Produto\MaiorEMenor;
 use PHPUnit_Framework_TestCase as PHPUnit;
 
 class MaiorEMenorTest extends PHPUnit
 {
-	public function testOrdemDecrescente()
-	{
-		$carrinho = new CarrinhoDeCompras();
+    public function testOrdemDecrescente()
+    {
+        $carrinho = new CarrinhoDeCompras();
 
-		$carrinho->adiciona(
-			new Produto("Geladeira", 450.00)
-		);
-		$carrinho->adiciona(
-			new Produto("Loquidificador", 250.00)
-		);
-		$carrinho->adiciona(
-			new Produto("Jogo de pratos", 70.00)
-		);
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+        $carrinho->adiciona(new Produto("Liquidificador", 250.00));
+        $carrinho->adiciona(new Produto("Jogo de pratos", 70.00));
 
-		$maiorMenor = new MaiorEMenor();
-		$maiorMenor->encontra($carrinho);
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
 
-		$this->assertEquals(
-			"Jogo de pratos",
-			$maiorMenor->getMenor()->getNome()
-		);
-		$this->assertEquals(
-			"Geladeira",
-			$maiorMenor->getMaior()->getNome()
-		);
-	}
+        $this->assertEquals(
+        	"Jogo de pratos",
+        	$maiorMenor->getMenor()->getNome()
+    	);
+        $this->assertEquals(
+        	"Geladeira",
+        	$maiorMenor->getMaior()->getNome()
+    	);
+    }
 }
