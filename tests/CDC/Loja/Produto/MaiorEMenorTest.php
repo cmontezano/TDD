104f4cbef7d31d24a824865a84f75858c95eb8ab
Anisio Neto <neto.df.pn@gmail.com>
2015-03-16 23:52:50 -0300

    Ajustando nomes incorretos

--- tests/CDC/Loja/Produto/MaiorEMenorTest.php
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -9,7 +9,7 @@ use CDC\Loja\Carrinho\CarrinhoDeCompras,
 	CDC\Loja\Produto\MaiorEMenor;
 use PHPUnit_Framework_TestCase as PHPUnit;
 
-class MaiorEMenor extends PHPUnit
+class MaiorEMenorTest extends PHPUnit
 {
 	public function testOrdemDecrescente()
 	{
